66d6507bf97e92891b86717e0a5d0d71da37f7a2
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2026-05-13 09:56:01 +0300

    ..F....... [DEV-4931] fixed a corner case with deeply nested expressions
    (cherry picked from commit 3b8c2e7a3dfd315e52619fd93b4363d2d3a27fb9)
    (cherry picked from commit 0aff2564d7ce0f2119e987d2692bbbcc5f47e4bf)
    (cherry picked from commit 9d2fa1151f10ddda9f0176532d3a3c01ffddcd2a)

--- ui/include/triggers.inc.php
+++ ui/include/triggers.inc.php
@@ -917,7 +917,7 @@ function getExpressionTree(string $expression, array $tokens, int $start, int $e
 							}
 
 							$expressions[] = getExpressionTree($expression, $tokens, $openSymbolNum, $closeSymbolNum,
-								$calls++
+								++$calls
 							);
 							$openSymbolNum = $i + $tokens[$i]['length'];
 							$operatorFound = true;
@@ -939,7 +939,7 @@ function getExpressionTree(string $expression, array $tokens, int $start, int $e
 		 * expression on the right.
 		 */
 		if ($operatorFound) {
-			$expressions[] = getExpressionTree($expression, $tokens, $openSymbolNum, $closeSymbolNum, $calls++);
+			$expressions[] = getExpressionTree($expression, $tokens, $openSymbolNum, $closeSymbolNum, ++$calls);
 
 			// Trim blank symbols in the beginning of the trigger expression.
 			$openSymbolNum = $start;
@@ -969,7 +969,7 @@ function getExpressionTree(string $expression, array $tokens, int $start, int $e
 				$openSymbolNum++;
 				$closeSymbolNum--;
 
-				$expressionTree = getExpressionTree($expression, $tokens, $openSymbolNum, $closeSymbolNum, $calls++);
+				$expressionTree = getExpressionTree($expression, $tokens, $openSymbolNum, $closeSymbolNum, ++$calls);
 			}
 			// No extra parentheses remain, return the result.
 			else {
